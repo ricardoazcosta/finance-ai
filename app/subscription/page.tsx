2f4b49ae6066bc3e72d1b8387fb5c630c775b8d7
ricardoazcosta <ricardozanete@gmail.com>
2026-01-03 03:41:04 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Page Subscription</h1>
+  );
+}
+
+export default Home;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return (<h1>Transaction Page</h1>);
+}
+
+export default Transactions;
