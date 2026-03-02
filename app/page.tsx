255af00a8c1aa7a7483383057d01961d91a42ee6
Ricardo Costa <ricardozanete@gmail.com>
2026-03-02 10:52:27 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
 	return (
 		<div className="flex w-screen items-center justify-center">
 			<h1 className="p-5 text-red-500">Page Home</h1>
+			<Button>Olá Bem Vindo</Button>
 		</div>
 	);
 };
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../_components/ui/button";
+
 const Transactions = () => {
 	return (
 		<div className="flex w-screen items-center justify-center">
-			<h1 className="p-5 text-red-500">Transaction Page</h1>;
+			<h1 className="p-5 text-red-500">Transaction Page</h1>
+			<Button/>
 		</div>
 	);
 };
